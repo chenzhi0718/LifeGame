19303c841e94b0652a8812e1e2810480d31ae12f
chenzhi0718 <chenzhi0718@hotmail.com>
2016-10-23 21:37:26 +0800

    msg5

---
+++ LifeGameTest.java
@@ -0,0 +1,37 @@
+package lifegame;
+
+import static org.junit.Assert.*;
+
+import java.awt.Color;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class LifeGameTest {
+
+
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	
+	@Test
+	public void testnextWorld() {
+		
+	}
+	
+	@Test
+	public void testInIt() {
+		
+	}
+	
+	@Test
+	public void testrun () {
+		
+        } 
+	
+	}
+
+
